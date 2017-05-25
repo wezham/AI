efa6d6ddf6c57b66c86b12be4d662093abfbf0f2
Wesley Hamburger <wezham1@gmail.com>
2017-05-25 12:27:18 +1000

    wip

--- Agent.java
+++ Agent.java
@@ -15,52 +15,23 @@ public class Agent {
    private State currentState;
    private Integer counter = 0;
    private char lastAction;
-   //represents the absolute orientation of the agent (NSEW)
-   private Orientation orientation;
-   private Map map;
+
+   private Orientation absoluteOrientation;
+   private LinkedList<Character> moves = new LinkedList<Character>();
+
+   private void get_actions(char view[][]){
+     PathPlanner pathPlanner = new PathPlanner();
+     Point a = new Point(2, 2, '^');
+     Point b = new Point(4, 4, view[4][4]);
+     //should be map
+     Search s = new Search(view);
+     // Given the map = run method that generates aStar searches two boundary points
+     LinkedList<Point> path = s.aStar(a, b);
+   }
 
 
    public char get_action( char view[][] ) {
-
-   if (counter < 20) {
-      //if it is our first move
-      if (currentState == null) {
-         currentState = new State(view, new Point(0, 0, view[2][2]));
-         orientation = new Orientation();
-      }
-      else {
-         //create a new state based and caluclate new position
-         State newState = new State(view, absolutePointCalculator(lastAction));
-         //replace state and update child parent relationship btw old and new
-         currentState.setChildState(newState);
-         newState.setParentState(currentState);
-         currentState = newState;
-      }
-        //determine best move
-        Move m = this.currentState.findBestMove();
-        lastAction = m.getAction();
-        orientation.updateOrientation(lastAction);
-
-        if (counter == 0 ){
-          map = new Map(view);
-          map.print();
-          System.out.println(map);
-       } else if (counter == 4) {
-          map.update(view, currentState.getAbsolutePoint(), orientation);
-         //  map.print();
-       }
-
-       System.out.println("Move Made: " + m);
-       System.out.println("Absolute Center Before Move: " + currentState.getAbsolutePoint());
-       System.out.println("----------");
-
-        counter ++;
-
-        return lastAction;
-     }
-
-
-      return 'F';
+     return 'F';
    }
 
 
@@ -172,11 +143,50 @@ public class Agent {
    }
 }
 
-// if (counter < 2) {
-//    this.currentState = new State(view, ' ', null);
-//    LinkedList<State> optionalMoves = this.currentState.generateChildren();
-//    System.out.println("Move Made: " + optionalMoves.peek());
-//    System.out.println("----------");
-//    counter ++;
-//    return optionalMoves.peek().getAction();
-// }
+/*
+//represents the absolute orientation of the agent (NSEW)
+//  private Orientation orientation;
+//  private Map map;
+//
+//
+//  public char get_action( char view[][] ) {
+//
+//  if (counter < 20) {
+//     //if it is our first move
+//     if (currentState == null) {
+//        currentState = new State(view, new Point(0, 0, view[2][2]));
+//        orientation = new Orientation();
+//     }
+//     else {
+//        //create a new state based and caluclate new position
+//        State newState = new State(view, absolutePointCalculator(lastAction));
+//        //replace state and update child parent relationship btw old and new
+//        currentState.setChildState(newState);
+//        newState.setParentState(currentState);
+//        currentState = newState;
+//     }
+//       //determine best move
+//       Move m = this.currentState.findBestMove();
+//       lastAction = m.getAction();
+//       orientation.updateOrientation(lastAction);
+//
+//       if (counter == 0 ){
+//         map = new Map(view);
+//         map.print();
+//         System.out.println(map);
+//      } else if (counter == 4) {
+//         map.update(view, currentState.getAbsolutePoint(), orientation);
+//        //  map.print();
+//      }
+//
+//      System.out.println("Move Made: " + m);
+//      System.out.println("Absolute Center Before Move: " + currentState.getAbsolutePoint());
+//      System.out.println("----------");
+//
+//       counter ++;
+//
+//       return lastAction;
+//    }
+//
+//
+*/
